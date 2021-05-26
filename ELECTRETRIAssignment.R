25a13b13f751d208eda4fdf5bfcdd9e16071735d
Xoogsede <34721105+Xoogsede@users.noreply.github.com>
2021-05-26 13:38:09 +0200

    Delete ELECTRETRIAssignment.R

--- ELECTRETRIAssignment.R
+++
@@ -1,8 +0,0 @@
-ELECTRETRIAssignment <- function(performanceTable, 
-                                 indefferenceThresholdVector, preferenceThresholdVector, vetosThresholdVector=NULL,
-                                 credibilityThreshold, cutThreshold =NULL?
-                                 affectationMethod = NULL,
-                                 categoriesLowerProfiles,  
-                                 categoriesRanks, criteriaWeights, criteriaMinMax,
-                                 alternativesIDs = NULL, criteriaIDs = NULL, categoriesIDs = NULL)
-                                 
