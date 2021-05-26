3cf37607c274df970cafe09aca8357e017aedf7f
Xoogsede <34721105+Xoogsede@users.noreply.github.com>
2021-05-26 11:18:14 +0200

    Create ELECTRETRIAssignment.R
    This is a modified version of the MRSort function wich allow to do multi criteria decision using the ELECTRE-TRI method.

---
+++ ELECTRETRIAssignment.R
@@ -0,0 +1,8 @@
+ELECTRETRIAssignment <- function(performanceTable, 
+                                 indefferenceThresholdVector, preferenceThresholdVector, vetosThresholdVector=NULL,
+                                 credibilityThreshold, cutThreshold =NULL?
+                                 affectationMethod = NULL,
+                                 categoriesLowerProfiles,  
+                                 categoriesRanks, criteriaWeights, criteriaMinMax,
+                                 alternativesIDs = NULL, criteriaIDs = NULL, categoriesIDs = NULL)
+                                 
